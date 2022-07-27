a01a429a78741f834356730c87745c3ea8960102
Aiden Somboon <aidensomboon07@gmail.com>
2022-07-27 16:45:36 +0800

    V1.2.5

--- bin/cli.js
+++ bin/cli.js
@@ -13,37 +13,37 @@ const runCommand = (cmd) => {
     return true
 }
 
-let repoName = process.argv[2];
-if(!repoName) {
-    console.error(chalk.red('Please provide a repository name.'));
+let folderName = process.argv[2];
+if(!folderName) {
+    console.error(`${chalk.red('Please provide an installation folder name.')} ${chalk.blueBright('(or use "." to install in the current directory)')}`);
     process.exit();
 }
-if(repoName !== repoName.toLowerCase()) {
-    console.error(chalk.red('Repository name must be lowercase.'));
+if(folderName !== folderName.toLowerCase()) {
+    console.error(chalk.red('Folder name must be lowercase.'));
     process.exit();
 }
 
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/QVGK/vite-react ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/QVGK/vite-react ${folderName}`;
+const installDepsCommand = `cd ${folderName} && npm install`;
 
-log(chalk.blueBright(`Cloning Repository as ${repoName}`));
+log(chalk.blueBright(`Cloning Repository into ${folderName}`));
 const checkedOut = runCommand(gitCheckoutCommand);
 
 if(!checkedOut) {
-    log(chalk.red(`Failed to clone repository as ${repoName}`));
+    log(chalk.red(`Failed to clone repository into ${folderName}`));
     process.exit();
 }
 
-log(chalk.blueBright(`Installing Dependencies for ${repoName}`));
+log(chalk.blueBright(`Installing Dependencies for ${folderName}`));
 const depsInstalled = runCommand(installDepsCommand);
 if(!depsInstalled) {
-    log(chalk.red(`Failed to install dependencies for ${repoName}`));
+    log(chalk.red(`Failed to install dependencies for ${folderName}`));
     process.exit();
 }
 
 log(chalk.blueBright(`Welcome to QVGK's Vite Template for React`));
 log()
-log(chalk.blueBright('This template makes the default Vite template much more smooth.'));
+log(chalk.blueBright('This is an edited version of the default Vite template.'));
 log()
 log(chalk.blueBright('To start, run:'));
-log(chalk.green(`cd ${repoName} && npm start`));
+log(chalk.green(`cd ${folderName} && npm run dev`));
