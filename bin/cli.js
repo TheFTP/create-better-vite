33008101a2765565d967a834dae761ab95ee1e6d
Aiden Somboon <aidensomboon07@gmail.com>
2022-07-28 11:13:41 +0800

    V1.4.2

--- bin/cli.js
+++ bin/cli.js
@@ -66,7 +66,6 @@ const checkedOut = runCommand(gitCheckoutCommand);
 
 if(!checkedOut) {
     if(folderName === '.') {
-        log(chalk.red('Failed to clone repository into the current directory.'));
         log(chalk.red('Failed to clone repository into the current directory. (is there a hidden .git folder?)'));
     } else {
         log(chalk.red(`Failed to clone repository into ${folderName}`));
