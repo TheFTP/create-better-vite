128e55886ffeed5afafff736d7ed82543f589fe5
Aiden Somboon <aidensomboon07@gmail.com>
2022-07-31 20:22:22 +0800

    V2.0.1

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ import fs from 'fs';
 
 // Variables
 const log = console.log;
-const packageVersion = '2.0.0-dev12';
+const packageVersion = '2.0.1';
 
 const red = chalk.redBright;
 const noRed = chalk.hex('#ff0000')
